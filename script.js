c2f59ede52a1cf7a727036763c6251e09a70b7d6
Demi Adepoju <33731489+davidmaybel@users.noreply.github.com>
2025-05-08 03:17:57 -0600

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,7 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+const toggle = document.getElementById('theme-toggle');
+toggle.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+});
